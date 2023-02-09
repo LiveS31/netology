c13e5afa4a73c1b95b8d3c82e40ff573a86866d0
Sergey P <ponomarevsergej@gmail.com>
2023-02-09 22:56:31 +0300

    script2

--- base_data/dz2/Script-1.sql
+++ base_data/dz2/Script-1.sql
@@ -1,38 +1,38 @@
 -- Ok
 -- Создание таблиц
+-- сборник
 create table if not exists compilacion(
 	id_compilacion  INTEGER primary key,
 	names  text not null,
 	year_output INTEGER
 );
-
+-- альбом
 create table if not exists albums(
 	id  INTEGER primary key,
 	album TEXT not null,
 	year_output INTEGER
 );
-
+-- исполнитель
 create table if not exists executors (
 	id  INTEGER primary key,
-	executors text not null
+	name text not null
 );
-
+-- жанр
 create table if not exists genre(
 	id  INTEGER primary key,
 	genre VARCHAR(50) not null
 );
-
+-- песня
 create table if not exists song(
 	id INTEGER primary key,
 	album_id INTEGER not null references albums(id),
-	album_id2 INTEGER references albums(id),
 	song TEXT,
 	track_time FLOAT
 );
-
+-- промежуточные таблицы
 create table if not exists song_compilacion(
-	id_song INTEGER not null references song(id),
 	id_compilacion INTEGER not null references compilacion(id_compilacion),
+	id_song INTEGER not null references song(id),
 	constraint fpk primary key (id_song, id_compilacion)
 );
 
