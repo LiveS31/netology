2c0b4fcc4ead64f04fde2aa5b1fbaedc338e6286
Sergey P <ponomarevsergej@gmail.com>
2023-02-09 16:56:20 +0300

    script1!!

--- base_data/dz2/Script-1.sql
+++ base_data/dz2/Script-1.sql
@@ -1,31 +1,32 @@
+-- Ok
+-- Создание таблиц
 create table if not exists compilacion(
-	id_compilacion INTEGER primary key,
-	names text,
+	id_compilacion  INTEGER primary key,
+	names VARCHAR(60),
 	year_output INTEGER
 );
 
 create table if not exists albums(
-	id INTEGER primary key,
-	id_album INTEGER,
-	album TEXT,
+	id  INTEGER primary key,
+	album VARCHAR(60) not null,
 	year_output INTEGER
-	);
-
-create table if not exists executors(
-	id integer primary key,
-	id_executors INTEGER,
-	executors TEXT
 );
 
-create table if not exists title(
-	id INTEGER primary key,
-	genre TEXT
+create table if not exists executors (
+	id  INTEGER primary key,
+	executors VARCHAR(60)
+);
+
+create table if not exists genre(
+	id  INTEGER primary key,
+	genre VARCHAR(60) not null
 );
 
 create table if not exists song(
 	id INTEGER primary key,
-	id_song INTEGER not null references albums(id),
-	song VARCHAR(60),
+	album_id INTEGER not null references albums(id),
+	album_id2 INTEGER not null references albums(id),
+	song VARCHAR(100),
 	track_time FLOAT
 );
 
@@ -35,17 +36,14 @@ create table if not exists song_compilacion(
 	constraint fpk primary key (id_song, id_compilacion)
 );
 
-create  table if not exists albub_executors (
+create  table if not exists album_executors (
 	id_album INTEGER not null references albums(id),
 	id_executors integer not null references executors(id),
 	constraint fk primary key (id_album, id_executors)
 );
 
-create  table  if not exists title_executors(
-	id_genres integer not null references title (id),
-	id_executors integer not null references executors (id),
+create  table  if not exists genge_executors(
+	id_genres integer not null references genre(id),
+	id_executors integer not null references executors(id),
 	constraint pk primary key (id_genres, id_executors)
 );
-
--- Здравствуйте. подскажите почему если я оставляю album то он пишет вверху ID= 3
--- и не дает добавлять туда данные
