cc600d42313b4696a9830690558da7647a6b42f1
Sergey P <ponomarevsergej@gmail.com>
2023-02-10 14:47:36 +0300

    csript-1ch

--- base_data/dz3/Script-1.sql
+++ base_data/dz3/Script-1.sql
@@ -1,27 +1,31 @@
 -- Ok
 -- Создание таблиц
--- сборник
-create table if not exists compilacion(
-	id_compilacion  SERIAL primary key,
+-- Cборник
+create table if not exists compilation(
+	id_compilation  SERIAL primary key,
 	names  text not null,
 	year_output INTEGER
 );
+
 -- альбом
 create table if not exists albums(
 	id  SERIAL primary key,
 	album TEXT not null,
 	year_output INTEGER
 );
+
 -- исполнитель
 create table if not exists executors (
 	id  SERIAL primary key,
-	name text not null
+	name TEXT not null
 );
+
 -- жанр
 create table if not exists genre(
 	id  SERIAL primary key,
 	genre VARCHAR(50) not null
 );
+
 -- песня
 create table if not exists song(
 	id SERIAL primary key,
@@ -29,11 +33,12 @@ create table if not exists song(
 	song TEXT,
 	track_time FLOAT
 );
+
 -- промежуточные таблицы
-create table if not exists song_compilacion(
-	id_compilacion INTEGER not null references compilacion(id_compilacion),
+create table if not exists song_compilation(
+	id_compilation INTEGER not null references compilation(id_compilation),
 	id_song INTEGER not null references song(id),
-	constraint fpk primary key (id_song, id_compilacion)
+	constraint fpk primary key (id_song, id_compilation)
 );
 
 create  table if not exists album_executors (
