abb486662ed0353e4707da92822cd6e159287c61
Sergey P <ponomarevsergej@gmail.com>
2023-02-10 13:48:36 +0300

    csript-1ch

--- base_data/dz3/Script-1.sql
+++ base_data/dz3/Script-1.sql
@@ -1,44 +1,44 @@
 -- Ok
 -- Создание таблиц
+-- сборник
 create table if not exists compilacion(
-	id_compilacion  INTEGER primary key,
-	names VARCHAR(60),
+	id_compilacion  SERIAL primary key,
+	names  text not null,
 	year_output INTEGER
 );
-
+-- альбом
 create table if not exists albums(
-	id  INTEGER primary key,
-	album VARCHAR(60) not null,
+	id  SERIAL primary key,
+	album TEXT not null,
 	year_output INTEGER
 );
-
+-- исполнитель
 create table if not exists executors (
-	id  INTEGER primary key,
-	executors VARCHAR(60)
+	id  SERIAL primary key,
+	name text not null
 );
-
+-- жанр
 create table if not exists genre(
-	id  INTEGER primary key,
-	genre VARCHAR(60) not null
+	id  SERIAL primary key,
+	genre VARCHAR(50) not null
 );
-
+-- песня
 create table if not exists song(
-	id INTEGER primary key,
+	id SERIAL primary key,
 	album_id INTEGER not null references albums(id),
-	album_id2 INTEGER not null references albums(id),
-	song VARCHAR(100),
+	song TEXT,
 	track_time FLOAT
 );
-
+-- промежуточные таблицы
 create table if not exists song_compilacion(
-	id_song INTEGER not null references song(id),
 	id_compilacion INTEGER not null references compilacion(id_compilacion),
+	id_song INTEGER not null references song(id),
 	constraint fpk primary key (id_song, id_compilacion)
 );
 
 create  table if not exists album_executors (
-	id_album INTEGER not null references albums(id),
-	id_executors integer not null references executors(id),
+	id_album SERIAL not null references albums(id),
+	id_executors SERIAL not null references executors(id),
 	constraint fk primary key (id_album, id_executors)
 );
 
