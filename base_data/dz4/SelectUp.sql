8016257525fbc4c45f0b1e303e4bd64508b8801e
Sergey P <ponomarevsergej@gmail.com>
2023-02-13 00:49:07 +0300

    selectUP

--- base_data/dz4/SelectUp.sql
+++ base_data/dz4/SelectUp.sql
@@ -1,9 +1,18 @@
  
 -- 1. количество исполнителей в каждом жанре;
+select genre.genre, count(genre_executors.id_executors) from genre
+join genre_executors on genre.id = genre_executors.id_genres
+group by genre.genre
 
 -- 2.количество треков, вошедших в альбомы 2019-2020 годов;
+select count(song.id) from song
+join albums a  on song.album_id = a.id
+where  year_output >= 2019 and year_output <= 2022 ;
 
 -- 3.средняя продолжительность треков по каждому альбому;
+select a.album, avg(s.track_time) from albums a
+join song s on a.id  = s.album_id
+group by a.id ;
 
 -- 4.все исполнители, которые не выпустили альбомы в 2020 году;
 
