81e97c1b3c3240eadb528cfe781ee348df7198b1
Sergey P <ponomarevsergej@gmail.com>
2023-03-15 16:11:02 +0300

    dz_scraping

--- web_scraping/dz1_par.py
+++ web_scraping/dz1_par.py
@@ -1,44 +1,52 @@
-import requests
-import json
-from fake_headers import Headers
-from bs4 import BeautifulSoup
-
-hh_url = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
-
-def get_handres():
-    return Headers(browser='firefox', os='win').generate()
-
-main_padge = requests.get(hh_url, headers=get_handres()).text
-#print(main_padge)
-bs = BeautifulSoup(main_padge, features='lxml')
-list_hh = bs.find_all(class_="serp-item__title")
-
-a_temp = []
-a_temp1 = []
-for txt in list_hh:
-    a_temp.append({
-        'vaca': txt.text,
-        'link': txt['href']
-                })
-
-print(a_temp)
-a_a = []
-list_hh1 = bs.find_all('span',class_="bloko-header-section-3")
-for zp in list_hh1:
-    zzp = zp.text.replace("\u202f", " ")
-    a_a.append({
-        'zp': zzp}) # обход неразрывноного пробела в юникоде
-print(a_a)
-
-
-#     a_a.append(zp.text)
-# print (a_a)
-
-# with open('data_file.json', 'w', encoding='utf-8') as data:
-#     json.dump(zp.text, data, indent=2, ensure_ascii=False)
-
-
-
-
-
+mix_sort = {'adres': ['Москва, Кутузовская, Кутузовская, Кутузовский проспект, 32к1',
+           'Санкт-Петербург, Тарасова улица, 12/18',
+           'Москва',
+           'Москва',
+           'Санкт-Петербург, Выборгская, Выборгская набережная, 49Б',
+           'Москва, Кузнецкий мост, Лубянка, Трубная, Малый Кисельный '
+           'переулок, 1/9',
+           'Брест, Пионерская улица, 52',
+           'Москва, Западный административный округ, Можайский район, '
+           'Инновационный центр Сколково, улица Нобеля, 5',
+           'Москва'],
+ 'jobs': ['Сбер. IT',
+          'ООО Единая Информационная Система ЖКХ',
+          'ООО Фабрика Решений',
+          'WorkinGeeks',
+          'ITRestoran(ООО Титул)',
+          'АНО Инфокультура',
+          'ООО Арктический 12',
+          'JetLend',
+          '4CV Recruitment Services'],
+ 'link-on': ['https://spb.hh.ru/vacancy/75888105?from=vacancy_search_list&query=python',
+             'https://spb.hh.ru/vacancy/77871173?from=vacancy_search_list&query=python',
+             'https://spb.hh.ru/vacancy/70705466?from=vacancy_search_list&query=python',
+             'https://spb.hh.ru/vacancy/77716464?from=vacancy_search_list&query=python',
+             'https://spb.hh.ru/vacancy/78033814?from=vacancy_search_list&query=python',
+             'https://spb.hh.ru/vacancy/77873430?from=vacancy_search_list&query=python',
+             'https://spb.hh.ru/vacancy/77992483?from=vacancy_search_list&query=python',
+             'https://spb.hh.ru/vacancy/78083627?from=vacancy_search_list&query=python',
+             'https://spb.hh.ru/vacancy/77853688?from=vacancy_search_list&query=python'],
+ 'zap': ['з/п не указана',
+         'от 90 000 до 150 000 руб. на руки',
+         'от 60 000 до 220 000 руб. на руки',
+         'от 180 000 руб. на руки',
+         'до 150 000 руб. до вычета налогов',
+         'от 150 000 до 180 000 руб. на руки',
+         'от 2 500 до 4 500 USD до вычета налогов',
+         'до 150 000 руб. на руки',
+         'от 7 000 до 9 000 USD до вычета налогов']}
+from pprint import pprint
+temp = []
+for a in range (len(mix_sort['adres'])):
+    if 'Москва' in mix_sort['adres'][a] or 'Санкт-Петербург' in mix_sort['adres'][a]:
+        temp.append({
+           'адрес': (mix_sort['adres'][a]),
+            'работодатель': (mix_sort['jobs'][a]),
+            'ссылка на вакансию': (mix_sort['link-on'][a]),
+            'зарплата': (mix_sort['zap'][a])
+        })
+  #  else:
+   #     continue
+pprint(temp)
 
--- web_scraping/zd1_1.py
+++ web_scraping/zd1_1.py
@@ -1,21 +1,113 @@
 import requests
-import json
 from fake_headers import Headers
 from bs4 import BeautifulSoup
+import json
+from pprint import pprint
+
 
 hh_url = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
-
+link_vac = []
+zp = []
+list= []
+mix_sort =[]
+vscan_t = []
+sort_list = []
+jobs_all = []
+adr = []
 def get_handres():
     return Headers(browser='firefox', os='win').generate()
 
-def main_pg():
+def link_on_page():
+    # сделано для того, чтобы что - то менялось(print)
+    print("начата сортировка по линку с переходом на страницу вакансии с главной", end='')
     main_padge = requests.get(hh_url, headers=get_handres()).text
-    # print(main_padge)
     bs = BeautifulSoup(main_padge, features='lxml')
-    list_hh = bs.find_all('span', class_="bloko-header-section-3")
-    a = bs.find_all(class_="serp-item__title")
-    print(list_hh)
-    print((a))
-    for b in a:
-        print(b['href'])
-main_pg()
+    vacan_s = bs.find_all('a',  class_="serp-item__title")
+    for vacan in vacan_s:
+        link_vac.append(vacan['href'])# забор линков на вакансии
+        response_link_temp = requests.get(vacan['href'], headers=get_handres()).text
+        response_link = BeautifulSoup(response_link_temp, features='lxml') #переходим на
+                                          # начинаем искать на стравнице вакансии
+        desc_vak = response_link.find('div', {'data-qa':'vacancy-description'}) # считываем данные с
+        # класса 'div', словарь 'data-qa':'vacancy-description' по ссылки один раз
+        if (('Django'or'django' ) or ('Flask' or 'flask')) in desc_vak.text: # сортируем по требованию задания
+            # (не очень понятно почему если первая большая
+            # буква - корректно. а если маленькая нет
+            vscan_t.append('+')
+            #temp.append(desc_vak)
+        else:
+            vscan_t.append('-')
+# через zip делаем цикл и складывая их сравнивая одну = забираем другую
+    for a, b in  zip(vscan_t, link_vac):
+        if a == '+':
+            sort_list.append(b)
+    # сделано для того, чтобы что - то менялось
+    print('- ВЫПОЛНЕНО')
+    return sort_list
+def zp_vac():
+    print ('Сбор данных по зарплатам', end=" ")
+    for link in sort_list:# перебираем станицы по ссылке на вакансии
+        zp_links = requests.get(link, headers=get_handres()).text
+        zp_link = BeautifulSoup(zp_links, features='lxml')
+        zp_link_on = zp_link.find('span', class_="bloko-header-section-2 bloko-header-section-2_lite")
+        zp.append(zp_link_on.text.replace("\xa0", " ")) #добавлям и декодируем
+    print('- ВЫПОЛНЕНО')
+    return zp
+
+def adres():
+    print('получение адеса по ссылке на вакансию', end=' ')
+
+    for link in sort_list:
+        locals = requests.get(link, headers=get_handres()).text
+        localss = BeautifulSoup(locals, features='lxml')
+        local = localss.find('span', {'data-qa':"vacancy-view-raw-address"})
+        local1 = localss.find('p', {'data-qa': 'vacancy-view-location'})
+        if local != None :
+            adr.append(local.text)
+        if local1 !=None:
+            adr.append(local1.text)
+    print('- ВЫПОЛНЕНО')
+    return adr
+
+def jobs():
+    print('Сбор информации о работодателе', end=' ')
+    for link in sort_list:
+        job_link = requests.get(link, headers=get_handres()).text
+        jobs_links = BeautifulSoup(job_link, features='lxml')
+        #jobs_link = jobs_links.find('a', class_="bloko-link bloko-link_kind-tertiary")
+        #не очень понимаю как, но такая информация не в одном месте...
+        # они вроде друг - друга дублируют.... оставлю это
+        jobs_link = jobs_links.find('span', {'data-qa': "bloko-header-2"}).text
+        jobs_all.append(jobs_link.replace("\xa0", " "))
+        #print (jobs_link.text)
+    print('- ВЫПОЛНЕНО')
+    return jobs_all
+
+def mix_grup(sort_list_all, zp, adress, jobs):
+    print('создание словаря', end=' ')
+    mix_sort = ({
+        'link-on': sort_list_all,
+        'zap' : zp,
+        'adres' : adress,
+        'jobs' : jobs
+                })
+    for a in range(len(mix_sort['adres'])):
+        if 'Москва' in mix_sort['adres'][a] or 'Санкт-Петербург' in mix_sort['adres'][a]:
+            list.append({
+                'адрес': (mix_sort['adres'][a]),
+                'работодатель': (mix_sort['jobs'][a]),
+                'ссылка на вакансию': (mix_sort['link-on'][a]),
+                'зарплата': (mix_sort['zap'][a])
+            })
+    print ('- ВЫПОЛНЕНО')
+    return list
+
+
+
+if __name__ == '__main__':
+    link_on_page()
+    zp_vac()
+    adres()
+    jobs()
+    mix_grup(sort_list, zp, adr, jobs_all)
+    pprint(list)
