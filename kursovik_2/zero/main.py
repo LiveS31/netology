6911a8928525e4e36eed239ab81ecfec7658f5d3
MaryaAd <117576738+MaryaAd@users.noreply.github.com>
2023-04-06 10:05:50 +0300

    Add files via upload
    Add bot zero

---
+++ kursovik_2/zero/main.py
@@ -0,0 +1,30 @@
+from random import randrange
+
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from settings import KEY_GROUP
+
+
+def get_long_poll(token):
+    vk = vk_api.VkApi(token=token)
+    longpoll = VkLongPoll(vk)
+    return longpoll
+
+
+def write_msg(user_id, message):
+    vk_api.VkApi(token=KEY_GROUP).method('messages.send',
+                                         {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7)})
+
+
+for event in get_long_poll(KEY_GROUP).listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        if event.to_me:
+            request = event.text
+
+            if request == "привет":
+                write_msg(event.user_id, f"Хай, {event.user_id}")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
