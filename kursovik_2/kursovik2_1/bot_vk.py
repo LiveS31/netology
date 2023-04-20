128627161e1378b7cac2de41d5b85f4f8e19c4b1
Sergey P <ponomarevsergej@gmail.com>
2023-04-20 22:28:23 +0300

    -ок (нужно добавить id в таблицу)

--- kursovik_2/kursovik2_1/bot_vk.py
+++ kursovik_2/kursovik2_1/bot_vk.py
@@ -4,16 +4,12 @@ from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 #from token import GROUP_TOKEN
 
 import configparser
-import sqlalchemy
 config =configparser.ConfigParser()
 config.read('.pass.ini')
-#token_vk = config['vk']['KEY_GROUP']
 GROUP_TOKEN = config['vk']['KEY_GROUP']
 from vkinder import VKinder_get_info, VKinder_get_photo, get_user_param
 from vkinder import MessagesSend
-from ins_data import ins_data, ins_fav_data, ins_propose_data, select_fav_client, create_tables, ins_user_client
-engine = sqlalchemy.create_engine(config['dsn']['DSSN'])
-#create_tables(engine)
+from ins_data import ins_data, ins_fav_data, ins_propose_data, select_fav_client#, sel_user_data, ins_user_client
 
 vk_session = vk_api.VkApi(token=GROUP_TOKEN)
 start_keyboard = VkKeyboard(inline=True)
@@ -51,7 +47,7 @@ def bot():
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
                 message = event.text.lower()
-                print(f'main {message}')
+                print(f'Инфо: {message}')
 
                 if message == 'запрос':
                     write_msg(event.user_id, f'Введите город, возраст,\n'
@@ -72,7 +68,6 @@ def bot():
                     try:
                         req_err = False
                         message = ",".join(message.split(" ")) if len(message.split(',')) == 1 else message
-                        print(message)
                         if len(message.split(',')) != 3:
                             write_msg(event.user_id,
                                       'Введите три параметра поиска через запятую.')
@@ -90,8 +85,6 @@ def bot():
                             write_msg(event.user_id, 'Возрастные ограничения 16.')
                             req_err = True
                         if req_err is False:
-                            print('req_err is False:')
-                            print(f'sex {sex} age {age} city {str(city.title())} user id {event.user_id}')
                             # сохраняем пользовательские данные в таб. Users
                             ins_data(event.user_id, int(age), str(sex).lower(), str(city.title()))
                     except ValueError:
@@ -100,33 +93,24 @@ def bot():
                         req_err = True
 
                 if req_err is False and message not in key_word:
-                    #req = sel_user_data(event.user_id)
-                   # print(f'sex {req[-1][2]} age {req[-1][1]} city {str(req[-1][3].title())} user id {event.user_id}')
-                    #info = VKinder_get_info(str(req[-1][2]).lower(), int(req[-1][1]),
-                                            # str(req[-1][3].title())).get_inf(event.user_id)
-                    print(f'sex {sex} age {age} city {str(city.title())} user id {event.user_id}')
                     info = VKinder_get_info(str(sex).lower(), int(age), str(city.title())).get_inf(event.user_id)
 
                     if info is None:
                         write_msg(event.user_id, 'Не найдено совпадений. Попробуйте еще раз!', main_keyboard)
-                        print(f'info None!!!!')
+                        print(f'Нет данных!!!!')
                     else:
                         if len(info) != 0:
                             write_msg(event.user_id, f'{info[0]} {info[1]} - {info[3]}')
-                            print(f'info {info}')
                             # добавляем полученные данные в таб. Propose
-                            print(f'{event.user_id} fff {info[0]} {info[1]} - {info[3]}')
-                            print(user_info)
+                            print(f'for id {event.user_id} : {info[0]} {info[1]} - {info[3]}')
                             # добавляем полученные данные в таб. Propose
                             ins_propose_data(event.user_id, info[2])
                             if [f"{event.user_id}, {sex}, {age}, {city}"] not in user_info:
                                 user_info.append([f"{event.user_id}, {sex}, {age}, {city}"])
                             photos = VKinder_get_photo(info[2]).get_photo_url()
 
-
                             if photos is not None:
                                 for i in photos:
-                                    print(i)
                                     MessagesSend(event.user_id, i).send_photo()
                             write_msg(event.user_id, f'Нажмите ❤, если нравится;\n'
                                                      f'"Далее" для продолжения поиска;\n'
@@ -145,12 +129,10 @@ def bot():
                 if message == '❤❤❤':
                     write_msg(event.user_id, f'❤ Ваш список избранных ❤')
                     favorites = select_fav_client(event.user_id)
-                    print(favorites)
                     for item in favorites:
                         write_msg(event.user_id, f'{item[2]} {item[1]} - {item[3]}')
                         for i in item[4].split(","):
                             i = i.replace('{', '').replace('}', '')
-                            print(f'fav - {i}')
                             MessagesSend(event.user_id, i).send_photo()
 
                     write_msg(event.user_id, f'❤❤❤end❤❤❤')
@@ -183,4 +165,5 @@ def run_bot():
                                                  f'Хотите с кем нибудь познакомиться?\n'
                                                  f'Нажмите "Старт"!!!', start_keyboard)
                         continue
-run_bot()
+if __name__ == '__main__':
+    run_bot()
--- kursovik_2/kursovik2_1/ins_data.py
+++ kursovik_2/kursovik2_1/ins_data.py
@@ -11,10 +11,10 @@ engine = sqlalchemy.create_engine(config['dsn']['DSSN'])
 Session = sessionmaker(bind=engine)
 session = Session()
 
-
-class Users(Base):
-    __tablename__ = 'users'
-    user_id = sq.Column(sq.Integer, primary_key=True)
+#Создание таблиц
+class Users(Base): # объявляем класс
+    __tablename__ = 'users' # создаем таблицу
+    user_id = sq.Column(sq.Integer, primary_key=True) # создаем названия граф + приоритет
     user_age = sq.Column(sq.Integer)
     user_gender = sq.Column(sq.VARCHAR(10))
     user_city = sq.Column(sq.VARCHAR(50))
@@ -23,7 +23,6 @@ class Users(Base):
 
 class FavoriteClients(Base):
     __tablename__ = 'favoriteclients'
-
     client_id = sq.Column(sq.Integer, primary_key=True)
     user_id = sq.Column(sq.Integer)
     client_name = sq.Column(sq.VARCHAR(20))
@@ -37,8 +36,8 @@ class FavoriteClients(Base):
 
 class Users_Client(Base):
     __tablename__ = 'users_client'
-    user_id = sq.Column(sq.Integer, sq.ForeignKey(Users.user_id), primary_key=True)
     favoriteclient_id = sq.Column(sq.Integer, sq.ForeignKey(FavoriteClients.client_id), primary_key=True)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey(Users.user_id), primary_key=True)
 
 
 class Users_Propose(Base):
@@ -46,15 +45,13 @@ class Users_Propose(Base):
     user_id = sq.Column(sq.Integer, sq.ForeignKey(Users.user_id), primary_key=True)
     prop_client_id = sq.Column(sq.Integer, primary_key=True)
 
-def create_tables(engine):
+def create_tables(engine): #функция созания и удаоения таблиц
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
     session.close()
-#create_tables(engine)
-    print ('111111111----')
 
-def ins_data(user_id, user_age, user_gender, user_city):#
-
+#Заполнение таблиц
+def ins_data(user_id, user_age, user_gender, user_city):
     if (session.query(Users).filter(Users.user_id == user_id).first() is None):
         session.add(Users
                 (
@@ -77,12 +74,9 @@ def ins_fav_data(user_id, client_id, client_name, client_surname, client_link, c
     if 3 - len(client_photo) != 0:
         for i in range(3-len(client_photo)):
             client_photo.append('ОТСУТСТВУЕТ')
-
-    print((client_photo))
     if (session.query(FavoriteClients).filter(FavoriteClients.client_id == client_id).first() is None):
         session.add(FavoriteClients
             (
-
             client_id=client_id,
             user_id=user_id,
             client_name=client_name,
@@ -91,12 +85,10 @@ def ins_fav_data(user_id, client_id, client_name, client_surname, client_link, c
             client_photos_1=client_photo[0],
             client_photos_2=client_photo[1],
             client_photos_3=client_photo[2]
-
         )
         )
     else:
         user = session.query(FavoriteClients).filter(FavoriteClients.client_id == client_id).first()
-
         user.client_id = client_id,
         user.user_id = user_id,
         user.client_name = client_name,
@@ -105,11 +97,22 @@ def ins_fav_data(user_id, client_id, client_name, client_surname, client_link, c
         user.client_photos_1 = client_photo[0],
         user.client_photos_2 = client_photo[1],
         user.client_photos_3 = client_photo[2]
+
+    if (session.query(Users_Client).filter(Users_Client.user_id == user_id).first() is None):#
+        session.add(Users_Client
+                (
+                user_id=user_id,
+                favoriteclient_id=client_id,
+                )
+            )
+    else:
+        user= session.query(Users_Client).filter(Users_Client.user_id== user_id).first()
+        user.user_id= user_id
+        user.favoriteclient_id=client_id
     session.commit()
-    print('add to favorite')
 
 
-def ins_user_client(user_id, fav_client_id):#
+def ins_user_client(user_id, fav_client_id):
     if (session.query(Users_Client).filter(Users_Client.user_id == user_id).first() is None):
         session.add(Users_Client
                 (
@@ -138,9 +141,9 @@ def ins_propose_data(user_id, client_id):
         user.user_id= user_id
         user.prop_client_id=client_id
     session.commit()
-    print('add to user_prop')
 
 
+#
 def sel_prop_data(user_id):
     conn = engine.connect()
     sel = select(Users_Propose).where(Users_Propose.user_id == user_id)
@@ -163,15 +166,6 @@ def select_fav_client(user_id):
     sel = select(FavoriteClients).where(FavoriteClients.user_id == user_id)
     res = conn.execute(sel)
     res_list_fav = ([i for i in res])
-    print (res_list_fav)
+    #print (res_list_fav)
     return res_list_fav
 
-#create_tables(engine)
-#select_fav_client(769656100)
-#b = ['gdgd']#['fsdfs1111','fgdfg' ]
-# ins_user_client(1, 1)
-#ins_fav_data(1, 2, 'dggdg', 'gddgd', 'dfgd', b)#, 'ewrwe', 'wrwrww')
-#ins_data(1, 33, 'dgd', 'djjjd')
-# ins_propose_data(1,2)
-#session.commit()
-#session.close()
