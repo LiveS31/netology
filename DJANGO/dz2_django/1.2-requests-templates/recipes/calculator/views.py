85b35f29d525e6b67f1dd73a771e3a2104572490
Sergey P <ponomarevsergej@gmail.com>
2023-05-04 23:24:07 +0300

    dz2

---
+++ DJANGO/dz2_django/1.2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+def home_view(request):
+    template_name = 'home.html'
+    temp = []
+    for item in DATA:
+        temp.append(item)
+    context = {'recipe': temp}
+    return render(request, template_name, context)
+
+# Напишите ваш обработчик. Используйте viview_recipeewDATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
